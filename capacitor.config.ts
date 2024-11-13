9323fb5cf0dab60dbee11dc668ab0df92e241bf1
IgorGabrielM <igorgabprofissional@gmail.com>
2024-11-12 22:31:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sun-save',
+  webDir: 'www'
+};
+
+export default config;
